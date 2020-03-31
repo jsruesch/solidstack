09b5669784b2c69e2e75a2a0ee1294eba212a9ee
jsruesch <jsruesch@gmail.com>
2020-03-31 10:51:58 -0500

    Code cleanup

--- inc/nav-links-m.php
+++ inc/nav-links-m.php
@@ -1,11 +1,9 @@
 
 <div class="full">
-
    <div class="full mobile-nav-grid">
       <a class="<?= ($current_page == 'work') ? 'navactive' : '' ?>" href="#0">
          Why SolidStack
       </a>
-
       <a class="<?= ($current_page == 'about') ? 'navactive' : '' ?>" href="#0">
          Docs
       </a>
@@ -14,111 +12,104 @@
    <div class="accordion full">
       <h2 class="accordion__items text-center">Products</h2>
       <div class="accordion__content">
-         <div class="full">
-
-            <!-- HTTP -->
-               <div class="full product-card http relative">
-                  <div class="bgs-outline"></div>
-                  <div class="pc-content">
-                     <div class="full">
-                     <p class="card-title text-white">
-                        SolidStack.<span class="text-teal text-exbold">Http</span>
-                     </p>
-                     </div>
-                  </div><!-- CLOSE: .pc-content -->
-               </div><!-- END: HTTP -->
-
-            <!-- Rest -->
-               <div class="full product-card rest relative">
-                  <div class="bgs-outline"></div>
-                  <div class="pc-content">
-                     <p class="card-title text-white">
-                     SolidStack.<span class="text-pink text-exbold">Rest</span>
-                     </p>
-                  </div><!-- CLOSE: .pc-content -->
-               </div><!-- END: Rest -->
-
-            <!-- Coroutines -->
-               <div class="full product-card coroutines relative">
-                  <div class="bgs-outline"></div>  
-                  <div class="pc-content">
-                     <p class="card-title text-white">
-                     SolidStack.<span class="text-orange text-exbold">Coroutines</span>
-                     </p>
-                  </div><!-- CLOSE: .pc-content -->
-               </div><!-- END: Coroutines -->
-
-            <!-- Ioc -->
-               <div class="full product-card ioc relative">
-                  <div class="bgs-outline"></div>
-                  <div class="pc-content">
-                     <p class="card-title text-white">
-                     SolidStack.<span class="text-greena text-exbold">Ioc</span>
-                     </p>
-                  </div><!-- CLOSE: .pc-content -->
-               </div><!-- END: Ioc -->
-
-            <!-- Mvc -->
-               <div class="full product-card mvc relative">
-                  <div class="bgs-outline"></div>
-                  <div class="pc-content">
-                     <p class="card-title text-white">
-                     SolidStack.<span class="text-yellow text-exbold">Mvc</span>
-                     </p>
-                  </div><!-- CLOSE: .pc-content -->
-               </div><!-- END: Mvc -->
-
-            <!-- Logging -->
-               <div class="full product-card logging relative">
-                  <div class="bgs-outline"></div>
-                  <div class="pc-content">
-                     <p class="card-title text-white">
-                        SolidStack.<span class="text-purple text-exbold">Logging</span>
-                     </p>
+      <div class="full">
+         <!-- HTTP -->
+            <div class="full product-card http relative">
+               <div class="bgs-outline"></div>
+               <div class="pc-content">
+                  <div class="full">
+                  <p class="card-title text-white">
+                     SolidStack.<span class="text-teal text-exbold">Http</span>
+                  </p>
                   </div>
-               </div><!-- END: Logging -->
+               </div><!-- CLOSE: .pc-content -->
+            </div><!-- END: HTTP -->
 
-            <!-- Websockets -->
-               <div class="full product-card websockets relative">
-                  <div class="bgs-outline"></div>
-                  <div class="pc-content">
-                     <p class="card-title text-white">
-                     SolidStack.<span class="text-reda text-exbold">WebSockets</span>
-                     </p>
-                  </div><!-- CLOSE: .pc-content -->
-               </div><!-- END: Websockets -->
+         <!-- Rest -->
+            <div class="full product-card rest relative">
+               <div class="bgs-outline"></div>
+               <div class="pc-content">
+                  <p class="card-title text-white">
+                  SolidStack.<span class="text-pink text-exbold">Rest</span>
+                  </p>
+               </div><!-- CLOSE: .pc-content -->
+            </div><!-- END: Rest -->
 
-            <!-- Localization -->
-               <div class="full product-card localization relative">
-                  <div class="bgs-outline"></div>
-                  <div class="pc-content">
-                     <p class="card-title text-white">
-                     SolidStack.<span class="text-bluea text-exbold">Localization</span>
-                     </p>
-                  </div><!-- CLOSE: .pc-content -->
-               </div><!-- END: Localization -->
+         <!-- Coroutines -->
+            <div class="full product-card coroutines relative">
+               <div class="bgs-outline"></div>  
+               <div class="pc-content">
+                  <p class="card-title text-white">
+                  SolidStack.<span class="text-orange text-exbold">Coroutines</span>
+                  </p>
+               </div><!-- CLOSE: .pc-content -->
+            </div><!-- END: Coroutines -->
 
-            </div>
+         <!-- Ioc -->
+            <div class="full product-card ioc relative">
+               <div class="bgs-outline"></div>
+               <div class="pc-content">
+                  <p class="card-title text-white">
+                  SolidStack.<span class="text-greena text-exbold">Ioc</span>
+                  </p>
+               </div><!-- CLOSE: .pc-content -->
+            </div><!-- END: Ioc -->
+
+         <!-- Mvc -->
+            <div class="full product-card mvc relative">
+               <div class="bgs-outline"></div>
+               <div class="pc-content">
+                  <p class="card-title text-white">
+                  SolidStack.<span class="text-yellow text-exbold">Mvc</span>
+                  </p>
+               </div><!-- CLOSE: .pc-content -->
+            </div><!-- END: Mvc -->
+
+         <!-- Logging -->
+            <div class="full product-card logging relative">
+               <div class="bgs-outline"></div>
+               <div class="pc-content">
+                  <p class="card-title text-white">
+                     SolidStack.<span class="text-purple text-exbold">Logging</span>
+                  </p>
+               </div>
+            </div><!-- END: Logging -->
+
+         <!-- Websockets -->
+            <div class="full product-card websockets relative">
+               <div class="bgs-outline"></div>
+               <div class="pc-content">
+                  <p class="card-title text-white">
+                  SolidStack.<span class="text-reda text-exbold">WebSockets</span>
+                  </p>
+               </div><!-- CLOSE: .pc-content -->
+            </div><!-- END: Websockets -->
+
+         <!-- Localization -->
+            <div class="full product-card localization relative">
+               <div class="bgs-outline"></div>
+               <div class="pc-content">
+                  <p class="card-title text-white">
+                  SolidStack.<span class="text-bluea text-exbold">Localization</span>
+                  </p>
+               </div><!-- CLOSE: .pc-content -->
+            </div><!-- END: Localization -->
+
          </div>
       </div>
+   </div>
 
       <div class="full mobile-nav-grid">
-
          <a class="<?= ($current_page == 'contact') ? 'navactive' : '' ?> full" href="#0">
             Contribute
          </a>
-
          <a class="<?= ($current_page == 'contact') ? 'navactive' : '' ?> full" href="#0">
             Contact
          </a>
-
          <a class="<?= ($current_page == 'contact') ? 'navactive' : '' ?>" href="#0">
             <i class="fab fa-github"></i>
          </a>
-
       </div>
-
-
 </div>
 
 
--- index.php
+++ index.php
@@ -7,25 +7,20 @@
 
 <!-- Hero -->
     <section>
-
-
-            <div class="full bg-white relative hero">
-                <div class="bgs"></div>
-               
-
-                <div class="full grid-vertical-align-content">
-                    <div class="hero-title-container">
-                    <p class="hero-title">
-                    Build Solid <br />
-                    Architecture <br /> 
-                    in Unity</p>
-            
-                    <p class="hero-subtitle">Re-usable, solid architecture components for <span class="text-teal text-exbold">Unity</span> and <span class="text-teal text-exbold">C#</span></p>
-                </div>                        
-                </div>
-
+        <div class="full bg-white relative hero">
+            <div class="bgs"></div>
+            <div class="full grid-vertical-align-content">
+                <div class="hero-title-container">
+                <p class="hero-title">
+                Build Solid <br />
+                Architecture <br /> 
+                in Unity</p>
+        
+                <p class="hero-subtitle">Re-usable, solid architecture components for <span class="text-teal text-exbold">Unity</span> and <span class="text-teal text-exbold">C#</span></p>
+            </div>                        
             </div>
-        </section><!-- END: Hero -->
+        </div>
+    </section><!-- END: Hero -->
 
 <!-- Main Content -->
     <div class="wrapper relative bg-ddblue">
@@ -50,68 +45,67 @@
                     <?php include('./inc/product-cards/pc-localization.php') ?>
                 </div><!-- END: Product Card Grid -->
             </section><!-- END: Components -->
-
         </div><!-- CLOSE .wrapper-content -->
     </div><!-- END: Main Content / CLOSE: .wrapper -->
 
-    <!-- Architecture -->
-        <section class="wrapper relative bg-grad-white-pink">
-            <div class="wrapper-content">
-            <!-- Section Header -->
-                <div class="full grid-2col div grid-vertical-align-content pad30">
-                    <div class="full pad60">
-                        <img src="img/graphic-architecture.svg" alt="" class="grid-image">
-                    </div>
-                    <div class="full pad60">
-                    <p class="section-subheader text-pink mar10-bot">Enable Test-Driven Architecture</span></p>
-                    <p class="text-ddblue text-bold">Unit testing games is hard. SolidStack components are built with dependency injection and interface segregation in mind, and help streamling test-driven development for your project. </p>
-                    </div>
-                   
+<!-- Architecture -->
+    <section class="wrapper relative bg-grad-white-pink">
+        <div class="wrapper-content">
+        <!-- Section Header -->
+            <div class="full grid-2col div grid-vertical-align-content pad30">
+                <div class="full pad60">
+                    <img src="img/graphic-architecture.svg" alt="" class="grid-image">
                 </div>
-            </div><!-- CLOSE .wrapper-content -->
-        </section><!-- Architecture -->
+                <div class="full pad60">
+                <p class="section-subheader text-pink mar10-bot">Enable Test-Driven Architecture</span></p>
+                <p class="text-ddblue text-bold">Unit testing games is hard. SolidStack components are built with dependency injection and interface segregation in mind, and help streamling test-driven development for your project. </p>
+                </div>
+                
+            </div>
+        </div><!-- CLOSE .wrapper-content -->
+    </section><!-- Architecture -->
 
-    <!-- Applications -->
-        <section class="wrapper relative bg-grad-teal-white">
-            <div class="bgs-outline-dark"></div>
-            <div class="wrapper-content relative">
-            <!-- Section Header -->
-                <div class="full grid-2col div grid-vertical-align-content pad30">
-                    <div class="full pad60">
-                    <p class="section-subheader text-teal mar10-bot">Build Asynchronous Unity Applications</span></p>
-                    <p class="text-ddblue text-bold">Use full power of C# Tasks in Unity, with async/await syntax, progress tracking and cancellation tokens. Forget about coroutine-flavored callback hell.</p>
-                    </div>
-                    <div class="full pad60">
-                        <img src="img/graphic-applications.svg" alt="" class="grid-image">
-                    </div>
+<!-- Applications -->
+    <section class="wrapper relative bg-grad-teal-white">
+        <div class="bgs-outline-dark"></div>
+        <div class="wrapper-content relative">
+        <!-- Section Header -->
+            <div class="full grid-2col div grid-vertical-align-content pad30">
+                <div class="full pad60">
+                <p class="section-subheader text-teal mar10-bot">Build Asynchronous Unity Applications</span></p>
+                <p class="text-ddblue text-bold">Use full power of C# Tasks in Unity, with async/await syntax, progress tracking and cancellation tokens. Forget about coroutine-flavored callback hell.</p>
                 </div>
-            </div><!-- CLOSE .wrapper-content -->
-        </section><!-- Applications -->
+                <div class="full pad60">
+                    <img src="img/graphic-applications.svg" alt="" class="grid-image">
+                </div>
+            </div>
+        </div><!-- CLOSE .wrapper-content -->
+    </section><!-- Applications -->
 
-    <!-- Contribute -->
-        <section class="wrapper bg-ddblue">
-            <div class="wrapper-content">
-            <!-- Section Header -->
-                <div class="full center-callout pad60">
-                    <div class="center-callout-content">
-                        <div class="full mar10-bot">
-                            <p class="section-header text-white text-center"><span class="text-exbold">Contribute</span></p>
-                        </div>
-                        <div class="full mar20-bot">
-                            <p class="section-subheader text-teal text-center mar10-bot">Become part of the SolidStack community!</span></p>
-                            <p class="text-mblue text-center text-bold">Our Community is growing! Become part of the OpenSource community. Check out our components on GitHub</p>
-                        </div>
-                        <div class="full flex-center">
-                        <a href="#0" class="btn-b">
-                            <p class="">
-                                Get Started
-                            </p>
-                            </a>
-                        </div>
+<!-- Contribute -->
+    <section class="wrapper bg-ddblue">
+        <div class="wrapper-content">
+        <!-- Section Header -->
+            <div class="full center-callout pad60">
+                <div class="center-callout-content">
+                    <div class="full mar10-bot">
+                        <p class="section-header text-white text-center"><span class="text-exbold">Contribute</span></p>
+                    </div>
+                    <div class="full mar20-bot">
+                        <p class="section-subheader text-teal text-center mar10-bot">Become part of the SolidStack community!</span></p>
+                        <p class="text-mblue text-center text-bold">Our Community is growing! Become part of the OpenSource community. Check out our components on GitHub</p>
+                    </div>
+                    <div class="full flex-center">
+                    <a href="#0" class="btn-b">
+                        <p class="">
+                            Get Started
+                        </p>
+                        </a>
                     </div>
                 </div>
-            </div>            
-        </section><!-- END: Contribute -->
+            </div>
+        </div>            
+    </section><!-- END: Contribute -->
 
 <!-- Footer -->
 <?php include('./inc/footer.php') ?> 
